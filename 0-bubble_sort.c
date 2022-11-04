1a3c01ab085e6bac2e160cd0dea986af853ba3a0
harkinder-dark <victorvaddely@gmail.com>
2022-11-04 05:51:48 +0000

    nth day programming in C

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,29 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - sort bubble
+ * @array: array of integer
+ * @size: size of array
+*/
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j, k, tmp;
-    if (array != NULL)
+    size_t i, j;
+    int tmp;
+    if (array == NULL || size < 2)
     {
-        for (i = 0; array[i] != '\0'; i++);
-        if (i > 1)
+        return;
+    }
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size - 1; j++)
         {
-            for (j = 0; j < i; j++)
+            if (array[j] > array[j + 1])
             {
-                for (k = 0; k < i - 1; k++)
-                {
-                    if (array[k] > array[k + 1])
-                    {
-                        tmp = array[k];
-                        array[k] = array[k + 1];
-                        array[k + 1] = tmp;
-                    }
-                    print_array(array, i);
-                }
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
             }
+            print_array(array, size);
         }
     }
 }
