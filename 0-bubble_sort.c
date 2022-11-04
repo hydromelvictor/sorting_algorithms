3675385437ec387c03f17c5bc63dd76e822cf878
harkinder-dark <victorvaddely@gmail.com>
2022-11-04 05:34:43 +0000

    nth day programming in C

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
                     if (array[k] > array[k + 1])
                     {
                         tmp = array[k];
-                        array[k] = array[k + 1]
+                        array[k] = array[k + 1];
                         array[k + 1] = tmp;
                     }
                     print_array(array, i);
