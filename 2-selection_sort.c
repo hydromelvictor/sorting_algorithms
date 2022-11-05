edcb7743b1be0536fb5dc7cf8cb18276c24cd44a
harkinder-dark <victorvaddely@gmail.com>
2022-11-05 09:20:37 +0000

    nth day programming in C

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, k;
+    int min, n;
+    if (array == 0 || size < 2)
+    {
+        return;
+    }
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min = array[i];
+        n = 0;
+        for (j = i + 1; j < size; j++)
+        {
+            if (min > array[j])
+            {
+                min = array[j];
+                k = j;
+                n = 1;
+            }
+        }
+        if (n == 1)
+        {
+            array[k] = array[i];
+            array[i] = min;
+            print_array(array, size);
+        }
+        
+    }
+}
--- sort.h
+++ sort.h
@@ -22,5 +22,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void change(listint_t *lab, listint_t *fab, listint_t **head);
+void selection_sort(int *array, size_t size);
 
 #endif
