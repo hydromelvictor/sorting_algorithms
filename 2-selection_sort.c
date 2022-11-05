477f4f73884123f8eaf7ddf65543531a9c48d71b
harkinder-dark <victorvaddely@gmail.com>
2022-11-05 09:23:26 +0000

    nth day programming in C

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,33 +1,36 @@
 #include "sort.h"
 
+/**
+ * selection_sort - sort by selection
+ * @array: array
+ * @size: size for array
+*/
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, k;
-    int min, n;
-    if (array == 0 || size < 2)
-    {
-        return;
-    }
-
-    for (i = 0; i < size - 1; i++)
-    {
-        min = array[i];
-        n = 0;
-        for (j = i + 1; j < size; j++)
-        {
-            if (min > array[j])
-            {
-                min = array[j];
-                k = j;
-                n = 1;
-            }
-        }
-        if (n == 1)
-        {
-            array[k] = array[i];
-            array[i] = min;
-            print_array(array, size);
-        }
-        
-    }
+size_t i, j, k;
+int min, n;
+if (array == 0 || size < 2)
+{
+return;
+}
+for (i = 0; i < size - 1; i++)
+{
+min = array[i];
+n = 0;
+for (j = i + 1; j < size; j++)
+{
+if (min > array[j])
+{
+min = array[j];
+k = j;
+n = 1;
+}
+}
+if (n == 1)
+{
+array[k] = array[i];
+array[i] = min;
+print_array(array, size);
+}
+}
 }
