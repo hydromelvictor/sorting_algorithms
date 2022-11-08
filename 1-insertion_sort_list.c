a7996c7bac07084821416f849913feb035178eae
harkinder-dark <victorvaddely@gmail.com>
2022-11-08 10:01:29 +0000

    nth da programming in C

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,44 +1,53 @@
 #include "sort.h"
 
+/**
+ * change - swap function
+ * @back: back
+ * @nexter: next
+ * @head: header
+*/
 void change(listint_t *back, listint_t *nexter, listint_t **head)
 {
-    back->next = nexter->next;
-    if (nexter->next != NULL)
-        nexter->next->prev = back;
-    
-    nexter->next = back;
-    
-    nexter->prev = back->prev;
-    if (back->prev != NULL)
-        back->prev->next = nexter;
-    else
-        *head = nexter;
-    
-    back->prev = nexter;
+back->next = nexter->next;
+if (nexter->next != NULL)
+nexter->next->prev = back;
+
+nexter->next = back;
+nexter->prev = back->prev;
+if (back->prev != NULL)
+back->prev->next = nexter;
+else
+*head = nexter;
+
+back->prev = nexter;
 }
 
+/**
+ * inertion_sort_list - sort insertion
+ * @list: list
+*/
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current = *list, *back;
-    if (*list == NULL || current->next == NULL)
-    {
-        return;
-    }
-    while (current != NULL)
-    {
-        back = current;
-        current = current->next;
-        if (current != NULL)
-        {
-            while (back != NULL)
-            {
-                if (back->n > current->n)
-                {
-                    change(back, current, list);
-                    print_list(*list);
-                }
-                back = back->prev;
-            }
-        }
-    }
+listint_t *current = *list, *back;
+if (*list == NULL || current->next == NULL)
+{
+return;
+}
+while (current != NULL)
+{
+back = current;
+current = current->next;
+if (current != NULL)
+{
+while (back != NULL)
+{
+if (back->n > current->n)
+{
+change(back, current, list);
+print_list(*list);
+}
+back = back->prev;
+}
+}
+}
 }
