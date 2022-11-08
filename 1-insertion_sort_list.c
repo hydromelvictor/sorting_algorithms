9b3f555cedcc8ba79dff64bd3fb41c2909988026
harkinder-dark <victorvaddely@gmail.com>
2022-11-08 09:56:17 +0000

    nth da programming in C

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,11 +6,15 @@ void change(listint_t *back, listint_t *nexter, listint_t **head)
     if (nexter->next != NULL)
         nexter->next->prev = back;
     
+    nexter->next = back;
+    
     nexter->prev = back->prev;
     if (back->prev != NULL)
         back->prev->next = nexter;
     else
         *head = nexter;
+    
+    back->prev = nexter;
 }
 
 void insertion_sort_list(listint_t **list)
@@ -24,14 +28,17 @@ void insertion_sort_list(listint_t **list)
     {
         back = current;
         current = current->next;
-        while (back != NULL)
+        if (current != NULL)
         {
-            if (back->n > current->n)
+            while (back != NULL)
             {
-                change(back, current, list);
-                print_list(*list);
+                if (back->n > current->n)
+                {
+                    change(back, current, list);
+                    print_list(*list);
+                }
+                back = back->prev;
             }
-            back = back->prev;
         }
     }
 }
