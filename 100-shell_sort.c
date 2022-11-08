66a133023b757f4ddee8eb77984907b0100734fa
harkinder-dark <victorvaddely@gmail.com>
2022-11-08 16:36:49 +0000

    nth day programming in C

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -6,23 +6,23 @@ int value;
 size_t i, n, j;
 n = 1;
 while (n < size)
-    n = 3 * n + 1;
+n = 3 * n + 1;
 
 n = n / 3;
 while (n != 0)
 {
-    for (i = n; i < size; i++)
-    {
-        value = array[i];
-        j = i;
-        while ((j > n - 1) && array[j - n] > value)
-        {
-            array[j] = array[j - n];
-            j = j - n;
-        }
-        array[j] = value;
-    }
-    print_array(array, size);
-    n = n / 3;
+for (i = n; i < size; i++)
+{
+value = array[i];
+j = i;
+while ((j > n - 1) && array[j - n] > value)
+{
+array[j] = array[j - n];
+j = j - n;
+}
+array[j] = value;
+}
+print_array(array, size);
+n = n / 3;
 }
 }
